64cc6dc7863c77f639932723835667a772ea5406
sandramusta <sandraude@gmail.com>
2026-09-22 12:35:40 +0300

    Initial project setup: Spring Boot skeleton with dev/prod profile configuration

---
+++ src/main/java/ee/sandra/wpc/WpcApplication.java
@@ -0,0 +1,13 @@
+package ee.sandra.wpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WpcApplication.class, args);
+    }
+
+}
